098ed6a9f33639d73a34b2e610f6dff7d08b8f75
Gusev Sergey Valerevich <sergey.v.gusev@rtmis.ru>
2026-01-16 14:53:34 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.9"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "ru.anseranser"
+version = "0.0.1-SNAPSHOT"
+description = "Paymaster app for NA groups"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.xerial:sqlite-jdbc:3.51.1.0")
+	compileOnly("org.projectlombok:lombok")
+	runtimeOnly("com.h2database:h2")
+	runtimeOnly("org.postgresql:postgresql")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/main/java/ru/anseranser/paymaster/PaymasterApplication.java
@@ -0,0 +1,13 @@
+package ru.anseranser.paymaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymasterApplication.class, args);
+	}
+
+}
